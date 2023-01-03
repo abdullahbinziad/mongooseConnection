4afb0d81c947da73d64f855f9502d55d13121c06
abdullahbinziad <abdullahbinziad@gmail.com>
2023-01-03 07:49:38 +0600

    successfully DB connected

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 const app = express()
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 const port = 3000
 
@@ -10,21 +11,63 @@ mongoose.set('strictQuery', true);
 
 
 //set default mongoose connection 
-mongoose.connect('mongodb://localhost:27017/helloDB',)
-.then(()=> console.log("Mongoose Connected"))
-.catch((error)=> {
+
+
+const connectDB = async ()=>{
+
+
+    try {
+       await mongoose.connect('mongodb://127.0.0.1:27017/helloDB',);
+       console.log("DB is connected");
+    } catch (error){
+        console.log("DB is not connected ")
+        console.log(error.message);
+        process.exit(1);
+    }
+ };
     
-console.log("Not Connected");
-console.log(error);
-})
+
+ //creating schema of MongoDb
+
+ const productSchema = new mongoose.Schema({
+    title: String,
+    price:Number,
+    description: String,
+    creatDate :{
+        type: Date,
+        default: Date.now,
+    },
+ });
+
+ //create products models 
+
+ const Product = mongoose.model("Products", productSchema);
 
 
 
-
+//creating route
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
+
+
+app.post("/products", async (req,res)=>{
+    try{
+ //get data from req body 
+const newProduct = new Product({
+     title: req.body.title,
+     price: req.body.price,
+     description: req.body.description
+});
+ const productData = await newProduct.save();
+res.status(201).send(productData);
+    }catch(error){
+        res.status(500).send({message: error.message});
+    }
+})
+
+app.listen(port,async () => {
   console.log(`Example app listening on port ${port}`)
+  await connectDB();
 })
